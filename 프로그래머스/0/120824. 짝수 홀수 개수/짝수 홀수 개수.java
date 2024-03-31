3b28d7fdce6f3eb32d0d1e541aa8302f31a2273b
박동훈 <161265029+thoreausawyer@users.noreply.github.com>
2024-03-31 19:18:45 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int num1 = 0;
+        int num2 = 0;
+        for(int i = 0; i <= num_list.length-1; i++){
+            if(num_list[i] % 2 != 0){
+                num1++;
+            }
+            else{
+                num2++;
+            }
+        };
+        answer[1] = num1;
+        answer[0] = num2;
+        return answer;
+    }
+}
